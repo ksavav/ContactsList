4624a1ea8824fe36ed164ecccd02cdb6341e6fd1
Ksawery Armatynski <a.xawery@gmail.com>
2024-02-17 21:36:41 +0100

    added edit option, its working

---
+++ api/DTO/UpdateDto.cs
@@ -0,0 +1,13 @@
+namespace api.DTO
+{
+    public class UpdateDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public string? NewEmail { get; set; }
+        public string Role { get; set; }
+        public string SpecificRole { get; set; }
+    }
+}
