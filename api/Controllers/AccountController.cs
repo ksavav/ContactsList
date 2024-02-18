34d83c3adaff61f034159c3ca916ec57fc8c3680
Ksawery Armatynski <a.xawery@gmail.com>
2024-02-18 16:27:46 +0100

    Added Authentication and authorization

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -3,11 +3,11 @@ using System.Text;
 using api.Data;
 using api.DTO;
 using api.Entities;
+using api.Services;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-
+using api.Services;
 namespace api.Controllers
 {
     [ApiController]
@@ -15,10 +15,12 @@ namespace api.Controllers
     public class AccountController : ControllerBase
     {
         private readonly DataContext _context;
+        private readonly TokenService _token;
         private readonly IMapper _mapper;
-        public AccountController(DataContext context, IMapper mapper)
+        public AccountController(DataContext context, TokenService token, IMapper mapper)
         {
             _mapper = mapper;
+            _token = token;
             _context = context;
         }
 
@@ -44,15 +46,16 @@ namespace api.Controllers
 
             await _context.SaveChangesAsync();
 
-            return new UserDto
+            return Ok(new UserDto
             {
                 Name = user.Name,
                 Surname = user.Surname,
                 Phone = user.Phone,
                 Email = user.Email,
                 Role = user.Role,
-                SpecificRole = user.SpecificRole
-            };
+                SpecificRole = user.SpecificRole,
+                Token = _token.CreateToken(user)
+            });
         }
 
         [HttpPost("login")]
@@ -73,11 +76,20 @@ namespace api.Controllers
             {
                 if(login_hash[i] != user.PasswordHash[i])
                 {
-                    return BadRequest("Password does not match");
+                    return Unauthorized("Password does not match");
                 }
             }
 
-            return Ok(user);
+            return Ok(new UserDto 
+            {
+                Name = user.Name,
+                Surname = user.Surname,
+                Phone = user.Phone,
+                Email = user.Email,
+                Role = user.Role,
+                SpecificRole = user.SpecificRole,
+                Token = _token.CreateToken(user)
+            });
         }
     }
 }
--- api/Program.cs
+++ api/Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
 using api.Data;
+using api.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,11 +15,41 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddCors();
+builder.Services.AddScoped<TokenService>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddAuthentication(x => 
+{
+    // defacto nie trzeba tego dawać, bo to defautl
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        // ValidIssuer = server
+        // ValidAudience = clinet
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"])),
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+// to dopiero po httpsredirection
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseAuthorization();
 
